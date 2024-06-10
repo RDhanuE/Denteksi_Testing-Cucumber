e0c5c24c13f34d38b785523a807b7068e4c64b23
Marwahkamilaahmad <Mymarwahmad@gmail.com>
2024-06-11 06:34:00 +0700

    DashboardAdminDokter object

--- src/main/java/object/dokter/DashboardAdminDokter.java
+++ src/main/java/object/dokter/DashboardAdminDokter.java
@@ -1,4 +1,17 @@
 package object.dokter;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class DashboardAdminDokter {
+    WebDriver driver;
+
+    public DashboardAdminDokter(WebDriver driver){
+        this.driver = driver;
+    }
+
+    public By getTombolPeriksa(){ return By.id("");}
+    public By getGambar(){ return By.xpath("/html/body/div/div/div/div/div/div/div/div/div/div/img");}
+
+
 }
