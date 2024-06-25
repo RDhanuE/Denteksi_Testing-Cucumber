047a556db0c5bdc0c17ea45a7de9a6d7d144e9e4
Marwahkamilaahmad <Mymarwahmad@gmail.com>
2024-06-25 16:42:22 +0700

    PeriksaSekarangSteps

--- src/test/java/stepDefinitions/Hooks.java
+++ src/test/java/stepDefinitions/Hooks.java
@@ -1,5 +1,8 @@
 package stepDefinitions;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import org.openqa.selenium.WebDriver;
@@ -8,19 +11,26 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Hooks {
 
     private static WebDriver driver;
+    public static ExtentReports extent;
+    public static ExtentTest test;
 
     @Before
     public static void setup() {
+        extent = ExtentReportManager.getInstance();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
+        test = extent.createTest("Setup Driver & Open Browser");
     }
 
     @After
     public static void terminate() {
         driver.quit();
+        test.log(Status.INFO, "Browser closed");
+        extent.flush();
     }
 
     public static WebDriver getDriver() {
         return driver;
+
     }
 }
