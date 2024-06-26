900fd1b0e11e8687d1587d3be7d86e9f52910c15
nekkuzuria <amandafarliana10@gmail.com>
2024-06-27 01:46:23 +0700

    add method getDeleteConfirmationButton()

--- src/main/java/object/admin/DaftarAdminObject.java
+++ src/main/java/object/admin/DaftarAdminObject.java
@@ -3,10 +3,10 @@ package object.admin;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class DaftarDataAdminObject {
+public class DaftarAdminObject {
     WebDriver driver;
 
-    public DaftarDataAdminObject(WebDriver driver){
+    public DaftarAdminObject(WebDriver driver){
         this.driver = driver;
     }
 
@@ -14,5 +14,7 @@ public class DaftarDataAdminObject {
     public By getSearchBarAdmin(){return By.xpath("//*[@id=\"table-admin_filter\"]/label/input");}
     public By getEditButton(){return By.id("btn-edit");}
     public By getDeleteButton(){return By.id("btn-delete");}
+    public By getDeleteConfirmationButton(){return By.xpath("/html/body/div[2]/div/div[3]/button[1]");}
+
 
 }
--- src/main/java/object/admin/DaftarPasienObject.java
+++ src/main/java/object/admin/DaftarPasienObject.java
@@ -3,10 +3,10 @@ package object.admin;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class DaftarDataPasienObject {
+public class DaftarPasienObject {
     WebDriver driver;
 
-    public DaftarDataPasienObject(WebDriver driver){
+    public DaftarPasienObject(WebDriver driver){
         this.driver = driver;
     }
 
@@ -14,6 +14,7 @@ public class DaftarDataPasienObject {
     public By getSearchBarPasien(){return By.xpath("//*[@id=\"table-anak_filter\"]/label/input");}
     public By getEditButton(){return By.xpath("/html/body/div/div/div/div/div[2]/div[2]/div/div[2]/div/table/tbody/tr/td[4]/a[1]");}
     public By getDeleteButton(){return By.id("btn-delete");}
+    public By getDeleteConfirmationButton(){return By.xpath("/html/body/div[2]/div/div[3]/button[1]");}
     public By getCetakQRButton(){return By.xpath("/html/body/div/div/div/div/div[2]/div[2]/div/div[2]/div/table/tbody/tr/td[4]/a[3]");}
 
 
