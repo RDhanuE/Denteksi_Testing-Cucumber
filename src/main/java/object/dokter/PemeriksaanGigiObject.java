2af9a5119f8d174b461184589b481165099f6561
nekkuzuria <amandafarliana10@gmail.com>
2024-06-27 02:04:13 +0700

    change method name

--- src/main/java/object/dokter/PemeriksaanGigiObject.java
+++ src/main/java/object/dokter/PemeriksaanGigiObject.java
@@ -14,6 +14,6 @@ public class PemeriksaanGigiObject {
     public By getNamaOrtuTextBox() {return By.id("nama_orangtua");}
     public By getNoWhatsappTextBox() {return By.id("no_whatsapp");}
     public By getUploadPhoto() {return By.xpath("/html/body/div/div/div/div[1]/div/div/div[2]/form/div[4]/div/div/input");}
-    public By scanQRButton() {return By.xpath("/html/body/div/div/div/div[1]/div/div/div[2]/form/div[5]/button[1]");}
-    public By periksaButton() {return By.xpath("/html/body/div/div/div/div[1]/div/div/div[2]/form/div[5]/button[3]");}
+    public By getScanQRButton() {return By.xpath("/html/body/div/div/div/div[1]/div/div/div[2]/form/div[5]/button[1]");}
+    public By getPeriksaButton() {return By.xpath("/html/body/div/div/div/div[1]/div/div/div[2]/form/div[5]/button[3]");}
 }
